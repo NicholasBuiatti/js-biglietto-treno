a416a0a3c6335152512d594c84cf830167648b9c
NicholasBuiatti <nic.buiatti@gmail.com>
2024-04-17 18:48:12 +0200

    fine con un css moooolto brutto ma abbozzato in pochi secondi

--- js/script.js
+++ js/script.js
@@ -1,26 +1,42 @@
-const km = prompt(`Quanti km intendi percorrere?`);
+const km = prompt('Quanti Km vuoi percorrere?'); 
+ //document.getElementById("kmDaPercorrere").value;
 
-const anni = prompt(`Indica la tua età`);
+const anni = prompt(`Quanti hanni hai??`);
+//document.getElementById("età").value;
 
 let bigliettoIntero = km * 0.21;
 
+let bigliettoScontato;
+
+document.getElementById("km").innerHTML = km;
+document.getElementById("età").innerHTML = anni;
+// // function functionToExecute() {
+    
+// // }
 if (anni < 18) {
 
     let scontoUnder = (bigliettoIntero * 20) / 100;
-    let bigliettoScontato = (bigliettoIntero - scontoUnder)
-    console.log(bigliettoScontato)
+    bigliettoScontato = (bigliettoIntero - scontoUnder);
+    
+    document.getElementById("prezzoTotale").innerHTML = bigliettoScontato.toFixed(2);
+    document.getElementById("sconto").innerHTML = `20%`;
+    
+    console.log(bigliettoScontato.toFixed(2));
     console.log(`Sconto del 20%`);
 
 } else if (18 <= anni && anni < 65) {
 
-    console.log(bigliettoIntero);
+    document.getElementById("prezzoTotale").innerHTML = bigliettoIntero;
+    document.getElementById("sconto").innerHTML = `Non hai diritto allo sconto`;
+    
+    console.log(bigliettoIntero.toFixed(2));
     console.log(`non hai diritto`);
 
 } else {
 
     let scontoOver = (bigliettoIntero * 40) / 100;
-    let bigliettoScontato = (bigliettoIntero - scontoOver);
-    console.log(bigliettoScontato);
-    console.log(`Sconto del 40%`);
-
+    bigliettoScontato = (bigliettoIntero - scontoOver);
+    
+    document.getElementById("prezzoTotale").innerHTML = bigliettoScontato.toFixed(2);
+    document.getElementById("sconto").innerHTML = `40%`;
 }
